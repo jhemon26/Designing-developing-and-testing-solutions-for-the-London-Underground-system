ce55f5b46692fa2f5ee32db6d047359d91b81def
Kemi <oluwakemiadekanbi16@gmail.com>
2025-11-03 00:36:53 +0000

    Updated task 1.B application of tfl data to use the Pandas module to parse directly from the Excel sheet

--- Individual Task/Task 01/Kemi/task1_B_App.py
+++ Individual Task/Task 01/Kemi/task1_B_App.py
@@ -1,24 +1,23 @@
 from open_address_hashtable import OpenAddressHashTable
 from  hash_functions import hashpjw
-#Method of how i did this
-#I copied the names of all the stations in each train line.
-#Pasted them into a text file
-#wrote code that goes through each line in the text file , convert them to string and add them to the list if they aren't already in it.
+import pandas as pd
+#Method of how I acquired the TFL station data:
+#Installed the panda module to parse the Excel sheet.
+#Choose the row containing all the station names
+#Added all stations names to a list. Avoiding any repetitions by checking if they were already in the list.
 
-f = open("stations")
+stations_xl = pd.read_excel("London Underground data.xlsx")
 
-# print(f.read())
 station_lst = []
-for x in f:
-    station = str(x).replace("\n","")
-    if station not in station_lst:
-        station_lst.append(station)
+for x in stations_xl["Harrow & Wealdstone"].values.tolist():
+    if x not in station_lst:
+        station_lst.append(x)
 
-station_hashtable = OpenAddressHashTable(270, hashpjw)
+station_hashtable = OpenAddressHashTable(280, hashpjw)
 for i in station_lst:
     station_hashtable.insert(i)
 
-print(station_hashtable)
+print(station_lst)
 
 def search_station():
     name = input("Enter a station name: ")
@@ -28,5 +27,3 @@ def search_station():
     return "Operational"
 
 print(search_station())
-# print(station_hashtable.search("Dollis Hill"))
-# print(station_hashtable.table[80])
