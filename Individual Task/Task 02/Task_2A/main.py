0c6d486a036c50df82be3087724651768e07e742
Riyah <riyahhussain9@gmail.com>
2025-11-18 16:16:36 +0000

    Refactor main.py for clarity and functionality
    Refactor the main.py file to improve readability and structure. Update edge insertion and path reconstruction logic.

--- Individual Task/Task 02/Task_2A/main.py
+++ Individual Task/Task 02/Task_2A/main.py
@@ -4,29 +4,64 @@
 # Graph: undirected, weighted (minutes)
 #Expected output: A -> B -> C -> D -> F, Total 9 minute
 
-from adjacency_list_graph import AdjacencyListGraph
-from dijkstra import dijkstra
 
-labels = ['A','B','C','D','E','F']
-idx = {v:i for i,v in enumerate(labels)}
-G = AdjacencyListGraph(len(labels), True, True)  # directed=True, weighted=True
+import os
+import sys
+
+here = os.path.dirname(__file__)
+
+sys.path.append(here)
+
+for name in os.listdir(here):
+    path = os.path.join(here, name)
+    if os.path.isdir(path):
+        sys.path.append(path)
+
+from dijkstra import dijkstra
+from adjacency_list_graph import AdjacencyListGraph
+
+stations = ["A", "B", "C", "D", "E", "F"]
 
 edges = [
-    ('A','B',2), ('A','C',4),
-    ('B','C',1),
-    ('C','D',4), ('C','E',5), ('C','F',8),
-    ('D','F',2),
-    ('E','F',2),
+    ("A", "B", 2),
+    ("A", "C", 4),
+    ("B", "C", 1),
+    ("C", "D", 4),
+    ("C", "E", 5),
+    ("C", "F", 8),
+    ("D", "F", 2),
+    ("E", "F", 2),
 ]
-for u,v,w in edges:  # undirected => add both directions
-    G.insert_edge(idx[u], idx[v], w)
-    G.insert_edge(idx[v], idx[u], w)
 
-d, pi = dijkstra(G, idx['A'])
+G = AdjacencyListGraph(card_V=len(stations), directed=False, weighted=True)
 
-# reconstruct A->F
-path, v = [], idx['F']
-while v is not None and v != -1:
-    path.append(labels[v]); v = pi[v]
-print("Shortest path:", " -> ".join(reversed(path)))
-print("Total time:", d[idx['F']], "minutes")
+
+for u_name, v_name, w in edges:
+    u = stations.index(u_name)
+    v = stations.index(v_name)
+    G.insert_edge(u, v, w)
+
+start_label = "A"
+end_label = "F"
+
+s = stations.index(start_label)
+t = stations.index(end_label)
+
+d, pi = dijkstra(G, s)
+
+path_indices = []
+v = t
+while True:
+    path_indices.append(v)
+
+    if v == s or pi[v] is None:
+        break
+    v = pi[v]
+
+path_indices.reverse()
+path_stations = [stations[i] for i in path_indices]
+
+print(
+    f"The shortest path from A-F is {d[t]} minutes & this is the station it has passed: "
+    + " -> ".join(path_stations)
+)
